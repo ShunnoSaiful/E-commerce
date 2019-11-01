21557087e872a57e11fd07b16fad0fbd536af282
Saiful Islam <sunnosaiful@gmail.com>
2019-11-01 17:36:01 +0600

    delete unnecessary file

--- care/src/inventory/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, inlineformset_factory, TextInput
-from .models import Inventory
-
-
-class InventoryForm(ModelForm):
-	class Meta:
-		model = Inventory
-		fields = '__all__'
--- care/src/invoice/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import InvoiceProduct,Invoice
-
-# Register your models here.
-admin.site.register(InvoiceProduct)
-admin.site.register(Invoice)
-
